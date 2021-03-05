4c34758e92ed51a478d9064c4abae0411352f713
Steven Ho <steven.yyyz@live.cn>
2021-03-05 17:57:56 +0800

    bgrl method added, input samples shuffled

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ args = parser.parse_args()
 # Data to estimate
 N = args.num_samples
 d = args.dimensions
-dv = 200.0 # deviation of two distributions
+dv = 20.0 # deviation of two distributions
 mean1 = np.zeros((d,))
 mean2 = np.zeros((d,))
 mean1[0] = dv
@@ -21,7 +21,7 @@ A = np.random.multivariate_normal(mean1, cov, N)
 B = np.random.multivariate_normal(mean2, cov, N)
 
 # WD estimating
-methods = ["wgan"]
+methods = ["wgan", "bgtf"]
 for m in methods:
     import methods
     method = getattr(methods, m)(d, N)
--- methods.py
+++ methods.py
@@ -17,7 +17,10 @@ class wgan(Method):
         self.disc = MLP(input_shape, hidden_dim=64, num_outputs=1).to(device=self.device)
         self.disc_optim = Adam(self.disc.parameters(), lr=0.002)
 
-    def update_parameters(self, As, Bs):
+    def update_parameters(self, As, Bs, shuffle=True):
+        if shuffle:
+            np.random.shuffle(As)
+            np.random.shuffle(Bs)
         As = torch.FloatTensor(As).to(self.device)
         Bs = torch.FloatTensor(Bs).to(self.device)
         VAs = self.disc(As)
@@ -47,3 +50,51 @@ class wgan(Method):
             loss = self.update_parameters(As, Bs)
             self.losses[i] = loss       
             self.vals[i] = self.estimate(As, Bs)         
+
+class bgtf(Method):
+    def __init__(self, input_shape, vol):
+        super(bgtf, self).__init__()
+        self.input_shape = input_shape
+        self.vol = vol
+        self.gamma = 1.0 # control the effect of softmax
+        self.losses = np.zeros((self.max_iter,))
+        self.vals = np.zeros((self.max_iter,))
+
+        self.device = torch.device("cuda")
+        self.mu = MLP(input_shape, hidden_dim=64, num_outputs=1).to(device=self.device)    
+        self.nu = MLP(input_shape, hidden_dim=64, num_outputs=1).to(device=self.device)    
+        self.tf_optim = Adam(list(self.mu.parameters()) + list(self.nu.parameters()), lr=0.002)   
+
+    def update_parameters(self, As, Bs, shuffle=True):
+        if shuffle:
+            np.random.shuffle(As)
+            np.random.shuffle(Bs)        
+        As = torch.FloatTensor(As).to(self.device)
+        Bs = torch.FloatTensor(Bs).to(self.device)
+        VAs = self.mu(As)
+        VBs = self.nu(Bs)
+
+        cost = torch.norm(As - Bs, p=2, dim=-1)
+        damping = VAs.squeeze() - VBs.squeeze() - cost
+        damping = self.gamma * torch.exp(damping / self.gamma)
+        loss = -VAs.mean() + VBs.mean() + damping.mean()
+
+        self.tf_optim.zero_grad()
+        loss.backward()
+        self.tf_optim.step()
+
+        return loss.item()
+
+    def estimate(self, As, Bs):
+        As = torch.FloatTensor(As).to(self.device)
+        Bs = torch.FloatTensor(Bs).to(self.device)
+        VAs = self.mu(As)
+        VBs = self.nu(Bs)
+        rv = torch.abs(VAs.mean() - VBs.mean())
+        return rv.squeeze().detach().cpu().numpy()
+
+    def train(self, As, Bs):
+        for i in range(self.max_iter):
+            loss = self.update_parameters(As, Bs)
+            self.losses[i] = loss       
+            self.vals[i] = self.estimate(As, Bs)  
